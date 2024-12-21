0a328de4b7d5e249a2cd0eb1368817adf0d3f9b5
Murilo R.B Silva <murilryanbarbosa@gmail.com>
2024-12-21 17:04:54 +0000

    [refactor]: changed query default value

--- pysimpleip/views.py
+++ pysimpleip/views.py
@@ -7,10 +7,10 @@ ipquery = IpQueryClient()
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
+    query = ipquery.own_ip()
+
     if request.method == 'POST':
         ip = request.form['ip']
-        query = ipquery.get_ip(ip)
-    elif request.method == 'GET':
-        query = ipquery.own_ip()
+        query = ipquery.get_ip(ip) if ip != '' else ipquery.own_ip()
     
     return render_template('homepage.html', ip_to_show=query.json())
